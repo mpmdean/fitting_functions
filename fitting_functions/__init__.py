6c094eac3d4cd95419c498d84ea7253363db8b95
Mark Dean <mpmdean@gmail.com>
2020-09-11 16:09:22 -0400

    forgot to add functions

--- fitting_functions/__init__.py
+++ fitting_functions/__init__.py
@@ -1,4 +1,5 @@
 from .lineshapes import (paramagnon, magnon, bose, make_gaussian_kernal,
                          convolve, zero2Linear, zero2Quad, antisymlorz,
                          plane2D, plane3D, plane3Dcentered, lorentzianSq2D,
-                         lorentzianSq2DRot, lorentzianSq3D, error)
+                         lorentzianSq2DRot, lorentzianSq3D, error,
+                         power_decay, power_decay_convolved)
